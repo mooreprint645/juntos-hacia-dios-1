a05969bf67e10e856b79591a0cf1bda4827919e3
mooreprint645 <mooreprint645@gmail.com>
2026-07-07 08:32:40 -0600

    Fix Supabase connection key

--- Supabase.js
+++ Supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://bmtgfbtoyxwrrnygsqcj.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtdGdmYnRveXh3cnJunezqcjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MjQwMTQsImV4cCI6MjA5ODAwMDAxNH0.7RF-h7yqT6gn-rQvtTDOVtxqn_vlVYlwAIusb1wPuLA";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtdGdmYnRveXh3cnJueWdzcWNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MjQwMTQsImV4cCI6MjA5ODAwMDAxNH0.7RF-h7yqT6gn-rQvtTDOVtxqn_vlVYlwAIusb1wPuLA";
 
 var supabaseClient = supabase.createClient(
   SUPABASE_URL,
